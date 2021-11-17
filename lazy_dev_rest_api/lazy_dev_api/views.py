ac23086303a06ab85a90122f0c3c261d14130354
Ian Lockhart <ianlockhart@Ians-MacBook-Pro.local>
2021-11-17 12:26:21 -0700

    finished endpoints and cors

--- lazy_dev_rest_api/lazy_dev_api/views.py
+++ lazy_dev_rest_api/lazy_dev_api/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import generics
+from .serializers import GuideSerializer
+from .models import Guide
+
+class GuideList(generics.ListCreateAPIView):
+    queryset = Guide.objects.all().order_by('id') # tell django how to retrieve all objects from the DB, order by id ascending
+    serializer_class = GuideSerializer # tell django what serializer to use
+
+class GuideDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Guide.objects.all().order_by('id')
+    serializer_class = GuideSerializer
