be40640f2b3a01a5ada2e15a138b69b7c3edd084
SoNesky <gueronlj@gmail.com>
2021-11-18 15:37:18 -0500

    display img files from db

--- lazy_dev_api/models.py
+++ lazy_dev_api/models.py
@@ -1,15 +1,15 @@
+from django.contrib.postgres.fields import ArrayField
 from django.db import models
-
-# Create your models here.
-class Guide(models.Model):
-    name = models.CharField(max_length=32)
-    author_id = models.IntegerField()
-    likes = models.IntegerField()
-    content = models.CharField(max_length=1024)
-    image = models.CharField(max_length=128)
-    rating = models.IntegerField()
 
 class User(models.Model):
     name = models.CharField(max_length=32)
     password = models.CharField(max_length=128)
     online = models.BooleanField()
+
+class Guide(models.Model):
+    name = models.CharField(max_length=32)
+    author_id = models.ForeignKey(User, null=True, on_delete= models.SET_NULL)
+    likes = models.IntegerField()
+    content = models.CharField(max_length=1024)
+    image = models.ImageField(upload_to='images', blank=True)
+    rating = models.IntegerField()
--- lazy_dev_rest_api/README.md
+++ lazy_dev_rest_api/README.md
@@ -1,1 +1,26 @@
 # Project_4_Backend
+
+#Uploading Images
+
+-pip install Pillow
+*this allows us to use media uploads
+
+-Add Media URL(file path) to setting.py
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+*This says when it gets in image it will be stored in a folder called 'media'
+
+-Add Media URL to Django URLs
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include ('main.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+ DEBUG must be set to True
+
+import os (if needed)
+from django.conf import settings (if needed)
+from django.conf.urls.static import static (if needed)
+
+**TODO: CREATE FORMS THAT TAKE IMAGE UPLOAD
