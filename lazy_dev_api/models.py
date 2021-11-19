8a2b872d4f69c98fc714ce567e6e1b11d760f082
SoNesky <gueronlj@gmail.com>
2021-11-19 09:55:16 -0500

    users added with hashed pw

--- lazy_dev_api/models.py
+++ lazy_dev_api/models.py
@@ -2,14 +2,15 @@ from django.contrib.postgres.fields import ArrayField
 from django.db import models
 
 class User(models.Model):
-    name = models.CharField(max_length=32)
+    name = models.CharField(max_length=32, unique=True)
     password = models.CharField(max_length=128)
     online = models.BooleanField()
 
 class Guide(models.Model):
-    name = models.CharField(max_length=32)
+    name = models.CharField(max_length=32, blank=True, null=True)
     author_id = models.ForeignKey(User, null=True, on_delete= models.SET_NULL)
-    likes = models.IntegerField()
-    content = models.CharField(max_length=1024)
-    image = models.ImageField(upload_to='images', blank=True)
-    rating = models.IntegerField()
+    likes = models.IntegerField(blank=True, null=True)
+    content = models.CharField(max_length=1024, blank=True, null=True)
+    image = models.CharField(max_length=1024, blank=True, null=True)
+    rating = models.IntegerField(blank=True, null=True)
+    imageFile = models.ImageField(upload_to='images', blank=True, null=True)
